3617995f1872b7ada42e50ad4f0d6118ce1be949
SlayerofevilYT <realslayerofevil@gmail.com>
2023-09-16 14:37:39 +1000

    Created code for climate_q1

--- climate_q1.py
+++ climate_q1.py
@@ -1,8 +1,25 @@
 import matplotlib.pyplot as plt
-        
+import sqlite3
+
+connection = sqlite3.connect("climate.db")
+cursor = connection.cursor()
+
+sqlCommand = "SELECT * FROM ClimateData"
+
+cursor.execute(sqlCommand)
+rows = cursor.fetchall()
+
+
 years = []
 co2 = []
 temp = []
+
+for row in rows:
+    year, amountOfCO2, temperature = row
+    years.append(year)
+    co2.append(amountOfCO2)
+    temp.append(temperature)
+
 
 plt.subplot(2, 1, 1)
 plt.plot(years, co2, 'b--') 
@@ -16,3 +33,6 @@ plt.ylabel("Temp (C)")
 plt.xlabel("Year (decade)") 
 plt.show() 
 plt.savefig("co2_temp_1.png") 
+
+connection.commit()
+connection.close()
